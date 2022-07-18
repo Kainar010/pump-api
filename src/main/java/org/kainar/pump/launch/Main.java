1a6ec56277b8ca285b5feba19ab6e3cde6fb4d93
Kainar010 <kainar0215@mail.ru>
2022-07-18 23:26:34 +0600

    hello world

--- src/main/java/org/kainar/pump/launch/Main.java
+++ src/main/java/org/kainar/pump/launch/Main.java
@@ -1,25 +1,25 @@
-package org.kainar.pump;
+package org.kainar.pump.launch;
 
 import java.io.File;
-import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.catalina.webresources.StandardRoot;
 import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.core.StandardContext;
 import org.apache.catalina.webresources.DirResourceSet;;
 
 public class Main {
     public static void main(String[] args) throws LifecycleException{
+        String webappDirLocation = "src/main/webapp/";
         Tomcat tomcat = new Tomcat();
         tomcat.setPort(8080);
         tomcat.getConnector();
-
-        Context ctx = tomcat.addWebapp("", new File("src/main/webapp").getAbsolutePath());
+        StandardContext ctx = (StandardContext) tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());   
+        File additionWebInfClasses = new File("target/classes");
         WebResourceRoot resources = new StandardRoot(ctx);
-        resources.addPreResources(
-                new DirResourceSet(resources, "/WEB-INF/classes", new File("target/classes").getAbsolutePath(), "/"));
-        ctx.setResources(resources);
-
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);       
         tomcat.start();
         tomcat.getServer().await();
     }
--- src/main/java/org/kainar/pump/servlet/JustServlet.java
+++ src/main/java/org/kainar/pump/servlet/JustServlet.java
@@ -1,15 +1,15 @@
-package org.kainar.pump;
+package org.kainar.pump.servlet;
 
 import java.io.Writer;
 import java.io.IOException;
 
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
 
-@WebServlet(urlPatterns = "/")
+@WebServlet(urlPatterns = "/",name = "JustServlet")
 public class JustServlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
